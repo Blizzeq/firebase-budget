bec77ccf4a7afa8ed3d3706c793bf5af526f0b50
Jakub Krasuski <kubamax4@gmail.com>
2022-10-24 13:00:45 +0200

    Fixed Updating Component. Now it works properly.

--- src/Components/EditBudgetForm.js
+++ src/Components/EditBudgetForm.js
@@ -24,24 +24,18 @@ function EditBudgetForm({
 
     const [selectedCategory, setSelectedCategory] = useState(budgets[0].category);
 
-    const [editBudgetName, setEditBudgetName] = useState(budgets[0].name);
-
-    const [editBudgetAmount, setEditBudgetAmount] = useState(budgets[0].amount);
-
-    const [editTotalBudget, setEditTotalBudget] = useState(budgets[0].totalBudget);
-
-
-    const handleEmptyForm = () => {
-            budgets.map((budget) => {
-                if (budget.name === selectedBudget) {
-                    setEditBudgetName(budget.name);
-                    setEditBudgetAmount(budget.amount);
-                    setEditTotalBudget(budget.totalBudget);
-                }
-
-                return null;
-            })
+    const handleNoChange = () => {
+        budgets.map((budget) => {
+            if (budget.name === selectedBudget) {
+                setBudgetName(budget.name);
+                setBudgetAmount(budget.amount);
+                setSelectedCategory(budget.category);
+                setTotalBudget(budget.totalBudget);
+            }
+            return null;
+        })
     }
+
 
     const handleSubmit = (event) => {
         const form = event.currentTarget;
@@ -49,108 +43,97 @@ function EditBudgetForm({
             event.preventDefault();
             event.stopPropagation();
         } else {
-            if(budgetName === ""){
-                setBudgetName(editBudgetName);
-            }
-
-            if(budgetAmount === 0){
-                setBudgetAmount(editBudgetAmount);
-            }else if(budgetAmount > editTotalBudget) {
-                setBudgetAmount(editTotalBudget);
-            }
-
-            if(totalBudget === 0){
-                setTotalBudget(editTotalBudget);
-            }
-
+            event.preventDefault();
             updateBudget(user, budgetName, setBudgets, selectedCategory, budgetAmount, selectedBudget, setEditBudget, totalBudget);
         }
         setValidated(true);
     };
 
     useEffect(() => {
-        handleEmptyForm();
+        handleNoChange();
     }, [selectedBudget]);
 
 
-    return (
-        <div className={'Dashboard-EditBudget-Form'}>
-            <MDBCard>
-                <Form noValidate validated={validated} onSubmit={handleSubmit} className={"editBudgetForm"}>
-                    <p className={'edit-info'}>Select budget</p>
-                    <Form.Select aria-label="Default select example" value={selectedBudget}
-                                 onChange={(e) => setSelectedBudget(e.target.value)}>
-                        {budgets.map((budget, key) => (
-                            <option key={key} id={budget.budgetId} value={budget.name}>{budget.name}</option>
-                        ))}
-                    </Form.Select>
-                    <p className={'edit-info'}>Edit budget name</p>
-                    <FloatingLabel
-                        controlId="floatingBudget"
-                        label={selectedBudget}
-                    >
-                        <Form.Control type="text" placeholder="Budget"
-                                      onChange={(e) => setBudgetName(e.target.value)}/>
-                        <Form.Control.Feedback>
-                            Looks good!
-                        </Form.Control.Feedback>
-                        <Form.Control.Feedback type="invalid">
-                            Please provide a new name.
-                        </Form.Control.Feedback>
-                    </FloatingLabel>
-                    <p className={'edit-info'}>Select budget category</p>
-                    <Form.Select aria-label="Default select example"
-                                 onChange={(e) => setSelectedCategory(e.target.value)}>
-                        <option>
-                            {budgets.map((budget) => (
-                                [budget.name === selectedBudget && budget.category]
-                            ))}
-                        </option>
-                        <option value={'Housing'}>Housing</option>
-                        <option value={'Transportation'}>Transportation</option>
-                        <option value={'Food'}>Food</option>
-                        <option value={'Utilities'}>Utilities</option>
-                        <option value={'Insurance'}>Insurance</option>
-                        <option value={'Medical/Dental'}>Medical/Dental</option>
-                        <option value={'Savings'}>Savings</option>
-                        <option value={'Other'}>Other</option>
-                    </Form.Select>
-                    <p className={'edit-info'}>Edit current budget amount</p>
-                    <FloatingLabel controlId="floatingAmount"
-                                   label={budgets.map((budget) => (
-                                       [budget.name === selectedBudget && budget.amount]
-                                   ))}
-                    >
-                        <Form.Control type="number" placeholder="Amount"
-                                      onChange={(e) => setBudgetAmount(e.target.valueAsNumber)}/>
-                        <Form.Control.Feedback>
-                            Looks good!
-                        </Form.Control.Feedback>
-                        <Form.Control.Feedback type="invalid">
-                            Please provide a new amount.
-                        </Form.Control.Feedback>
-                    </FloatingLabel>
-                    <p className={'edit-info'}>Edit maximum budget amount</p>
-                    <FloatingLabel controlId="floatingAmount"
-                                   label={budgets.map((budget) => (
-                                       [budget.name === selectedBudget && budget.totalBudget]
-                                   ))}
-                    >
-                        <Form.Control type="number" placeholder="Amount"
-                                      onChange={(e) => setTotalBudget(e.target.valueAsNumber)}/>
-                        <Form.Control.Feedback>
-                            Looks good!
-                        </Form.Control.Feedback>
-                        <Form.Control.Feedback type="invalid">
-                            Please provide a new amount.
-                        </Form.Control.Feedback>
-                    </FloatingLabel>
-                    <Button variant={"success"} type={"submit"} className={'btn-edit'}>Edit Budget</Button>
-                    <Button variant={"danger"} onClick={() => setEditBudget(!editBudget)}
-                            className={'btn-cancel'}>Cancel</Button>
-                </Form>
-            </MDBCard>
-        </div>
+    return (<div className={'Dashboard-EditBudget-Form'}>
+        <MDBCard>
+            <Form noValidate validated={validated} onSubmit={handleSubmit} className={"editBudgetForm"}>
+                <p className={'edit-info'}>Select budget</p>
+                <Form.Select aria-label="Default select example" value={selectedBudget}
+                             onChange={(e) => setSelectedBudget(e.target.value)}>
+                    {budgets.map((budget, key) => (
+                        <option key={key} id={budget.budgetId} value={budget.name}>{budget.name}</option>))}
+                </Form.Select>
+                {budgets.map((budget) => [
+                    budget.name === selectedBudget && (
+                        <>
+                            <p className={'edit-info'}>Edit budget name</p>
+                            <FloatingLabel
+                                controlId="floatingBudget"
+                                label={selectedBudget}
+                            >
+                                <Form.Control type="text" placeholder="Budget"
+                                              defaultValue={budget.name}
+                                              onChange={(e) => setBudgetName(e.target.value)}/>
+                                <Form.Control.Feedback>
+                                    Looks good!
+                                </Form.Control.Feedback>
+                                <Form.Control.Feedback type="invalid">
+                                    Please provide a new name.
+                                </Form.Control.Feedback>
+                            </FloatingLabel>
+                            <p className={'edit-info'}>Select budget category</p>
+                            <Form.Select aria-label="Default select example"
+                                         onChange={(e) => setSelectedCategory(e.target.value)}>
+                                <option>
+                                    {budgets.map((budget) => ([budget.name === selectedBudget && budget.category]))}
+                                </option>
+                                <option value={'Housing'}>Housing</option>
+                                <option value={'Transportation'}>Transportation</option>
+                                <option value={'Food'}>Food</option>
+                                <option value={'Utilities'}>Utilities</option>
+                                <option value={'Insurance'}>Insurance</option>
+                                <option value={'Medical/Dental'}>Medical/Dental</option>
+                                <option value={'Savings'}>Savings</option>
+                                <option value={'Other'}>Other</option>
+                            </Form.Select>
+                            <p className={'edit-info'}>Edit current budget amount</p>
+                            <FloatingLabel controlId="floatingAmount"
+                                           label={budgets.map((budget) => ([budget.name === selectedBudget && budget.amount]))}
+                            >
+                                <Form.Control type="number" placeholder="Amount"
+                                              defaultValue={budget.amount}
+                                              onChange={(e) => setBudgetAmount(e.target.valueAsNumber)}/>
+                                <Form.Control.Feedback>
+                                    Looks good!
+                                </Form.Control.Feedback>
+                                <Form.Control.Feedback type="invalid">
+                                    Please provide a new amount.
+                                </Form.Control.Feedback>
+                            </FloatingLabel>
+                            <p className={'edit-info'}>Edit maximum budget amount</p>
+                            <FloatingLabel controlId="floatingAmount"
+                                           label={budgets.map((budget) => ([budget.name === selectedBudget && budget.totalBudget]))}
+                            >
+                                <Form.Control type="number" placeholder="Amount"
+                                              defaultValue={budget.totalBudget}
+                                              onChange={(e) => setTotalBudget(e.target.valueAsNumber)}/>
+                                <Form.Control.Feedback>
+                                    Looks good!
+                                </Form.Control.Feedback>
+                                <Form.Control.Feedback type="invalid">
+                                    Please provide a new amount.
+                                </Form.Control.Feedback>
+                            </FloatingLabel>
+                            <Button variant={"success"} type={"submit"} className={'btn-edit'}>Edit Budget</Button>
+                            <Button variant={"danger"} onClick={() => setEditBudget(!editBudget)}
+                                    className={'btn-cancel'}>Cancel</Button>
+                        </>
+                    )
+                ])
+                }
+            </Form>
+        </MDBCard>
+    </div>
     );
 }
 
